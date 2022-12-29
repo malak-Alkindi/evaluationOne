51962c4d1fd14a2345dc011ddbc281c4980c2160
unknown <malakalkindi1995@gmail.com>
2022-12-29 02:30:48 -0800

    pdf creater reader and writer

--- src/evaluationOneProject/pdfMain.java
+++ src/evaluationOneProject/pdfMain.java
@@ -1,7 +1,10 @@
 package evaluationOneProject;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Random;
@@ -9,12 +12,14 @@ import java.util.Scanner;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 public class pdfMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-System.out.println("sffffdddddddsss");
+
 
 Scanner sc = new Scanner(System.in);
 ArrayList<String> usersInput = new ArrayList<>();
@@ -60,12 +65,32 @@ for(Integer i =0 ;i<100;i++) {
 	    document.close();
 	    outputStream.close();
 
-	    System.out.println("Pdf created successfully.");
+		
 	} catch (Exception e) {
 	e.printStackTrace();
 	}
 	
 	
+	
+	
 }
-	}}
+
+        PdfReader pdfReader = new PdfReader("File1.pdf");
+    
+    	int pages = pdfReader.getNumberOfPages(); 
+    	 
+    	//Iterate the pdf through pages.
+    	for(int i=1; i<=pages; i++) { 
+    	  //Extract the page content using PdfTextExtractor.
+    	  String pageContent = 
+    	  	PdfTextExtractor.getTextFromPage(pdfReader, i);
+     
+    	  //Print the page content on console.
+    	  System.out.println("Content on Page "
+    	  		              + i + ": " + pageContent);
+          }
+    }
+
+
+	}
 
